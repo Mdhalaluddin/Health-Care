5022aff7cd1c2f6e65920a9036812420360a123f
Halal1122 <mdhealaluddin505@gmail.com>
2023-10-07 09:02:54 +0600

    firebase config file added and firebase i

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4eJHvV5cWNzgmC4h6eivC87bhhbxSjZg",
+  authDomain: "health-care-29c3f.firebaseapp.com",
+  projectId: "health-care-29c3f",
+  storageBucket: "health-care-29c3f.appspot.com",
+  messagingSenderId: "696786261425",
+  appId: "1:696786261425:web:c523d2dd68c69b433b9990"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
