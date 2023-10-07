7ea0292dad28bc84d392f586552ebc750b00847d
Halal1122 <mdhealaluddin505@gmail.com>
2023-10-07 08:56:59 +0600

    navbar add all router added

---
+++ src/Router/Router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
+
+
+const myCrateRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+        ]
+    }
+
+
+
+])
+
+
+
+
+export default myCrateRouter;
