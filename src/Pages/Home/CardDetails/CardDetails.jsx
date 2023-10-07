a376c04a13fc10e4daa64334c627c05591bd1911
Halal1122 <mdhealaluddin505@gmail.com>
2023-10-07 14:15:20 +0600

    card details add

--- src/Pages/Home/CardDetails/CardDetails.jsx
+++ src/Pages/Home/CardDetails/CardDetails.jsx
@@ -3,14 +3,23 @@ import { useLoaderData, useParams } from "react-router-dom";
 
 const CardDetails = () => {
     const cards = useLoaderData();
-    console.log('card details',cards);
-    const {id} = useParams();
+    console.log('card details', cards);
+    const { id } = useParams();
     // const idInt = parseInt(id);
     const card = cards.find(card => card.id === id)
     console.log(card);
     return (
-        <div>
-            <h2 className="text-4xl">Card Details</h2>
+        <div className="container mx-auto w-full">
+            <div className="card card-compact w-1/2 bg-base-100 shadow-xl mt-12">
+                <figure><img src={card.image} alt="Shoes" className="w-full" /></figure>
+                <div className="card-body ">
+                    <h2 className="card-title text-center justify-center">{card.name}</h2>
+                    <p>{card.short_description}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
